79b3276008810af8b83bf8b18ce7637bd2babd15
pauloalonso1 <pauloa65@gmail.com>
2025-07-25 17:58:16 -0300

    sync

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
 import ShinyText from '@/components/TextAnimations/ShinyText/ShinyText';
-import DarkVeil from '@/components/DarkVeil'; // Adjust the import path as necessary';
+import DarkVeil from '@/components/DarkVeil/DarkVeil'; // Adjust the import path as necessary';
 
 const ComingSoon = () => {
   const [email, setEmail] = useState('');
